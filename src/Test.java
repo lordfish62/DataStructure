94ae4f1077ced07aed835f1219c059f226461d94
Jeff Liu <lordfish62@gmail.com>
2015-11-22 13:48:32 +0800

    no message

--- src/Test.java
+++ src/Test.java
@@ -1,33 +1,6 @@
-import nccu.vipl.jeff.stackandqueue.CircularQueueByArray;
-import nccu.vipl.jeff.stackandqueue.Queue;
-import nccu.vipl.jeff.stackandqueue.QueueByArray;
-import nccu.vipl.jeff.stackandqueue.QueueByLinkList;
-
-class Node {
-	int data;
-	Node next;
-}
 public class Test {
 	
 	public static void main(String[] args) {
-		int count = 0;
-		Node head;
-		Node sec = new Node();
-		Node trd = new Node();
-		Node four = new Node();
-		head = sec;
-		sec.data = 10;
-		sec.next = trd;
-		trd.data = 20;
-		trd.next = four;
-		four.data = 30;
-		four.next = head;
-		
-		Node p = head;
-		do {
-			count++;
-			p = p.next;
-		}while(p != head);
-		System.out.println(count);
+		System.out.println("Hello");
 	}
 }
