569c9982c92ad44d8b75da62ce371154cf261c7f
Jeff Liu <lordfish62@gmail.com>
2015-11-22 13:41:03 +0800

    no message

--- src/Test.java
+++ src/Test.java
@@ -3,34 +3,31 @@ import nccu.vipl.jeff.stackandqueue.Queue;
 import nccu.vipl.jeff.stackandqueue.QueueByArray;
 import nccu.vipl.jeff.stackandqueue.QueueByLinkList;
 
+class Node {
+	int data;
+	Node next;
+}
 public class Test {
+	
 	public static void main(String[] args) {
-		Queue qa = new CircularQueueByArray();
-		try {
-			qa.enQueue(10);
-			qa.enQueue(20);
-			qa.enQueue(30);
-			qa.enQueue(40);
-			qa.enQueue(50);
-			qa.enQueue(60);
-			qa.enQueue(70);
-			qa.enQueue(80);
-			qa.enQueue(90);
-			System.out.println(qa.deQueue());
-			System.out.println(qa.deQueue());
-			qa.enQueue(100);
-			qa.enQueue(110);
-			System.out.println(qa.deQueue());
-			System.out.println(qa.deQueue());
-			System.out.println(qa.deQueue());
-			System.out.println(qa.deQueue());
-			System.out.println(qa.deQueue());
-			System.out.println(qa.deQueue());
-			System.out.println(qa.deQueue());
-			System.out.println(qa.deQueue());
-			System.out.println(qa.deQueue());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		int count = 0;
+		Node head;
+		Node sec = new Node();
+		Node trd = new Node();
+		Node four = new Node();
+		head = sec;
+		sec.data = 10;
+		sec.next = trd;
+		trd.data = 20;
+		trd.next = four;
+		four.data = 30;
+		four.next = head;
+		
+		Node p = head;
+		do {
+			count++;
+			p = p.next;
+		}while(p != head);
+		System.out.println(count);
 	}
 }
